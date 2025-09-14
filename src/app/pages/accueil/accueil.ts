b048e67b9ef81e5146b9ae2e01ce35d59745ab03
Amin Handoyo Camélia <camhdyo@gmail.com>
2025-09-15 08:19:20 +1100

    Carrousel

--- src/app/pages/accueil/accueil.ts
+++ src/app/pages/accueil/accueil.ts
@@ -3,17 +3,34 @@ import { CommonModule, DatePipe } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Articles } from '../../services/articles/articles';
 import { Carrousel } from '../../services/carrousel/carrousel';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
   templateUrl: './accueil.html',
   styleUrl: './accueil.css',
-  imports: [CommonModule, RouterModule, DatePipe]
+  imports: [CommonModule, RouterModule, DatePipe, NgbCarouselModule]
 })
 export class Accueil implements OnInit {
   articles: any[] = [];
   carrouselPartenaires: any[] = [];
+  groupedPartenaires: any[][] = [];
+
+  slideConfig = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: false,
+    infinite: true,
+    centerMode: true,
+    responsive: [
+      { breakpoint: 900, settings: { slidesToShow: 2 } },
+      { breakpoint: 600, settings: { slidesToShow: 1 } }
+    ]
+  };
 
   constructor(
     private articlesService: Articles,
@@ -26,6 +43,15 @@ export class Accueil implements OnInit {
     });
     this.carrouselService.getPartenaires().subscribe((data: any[]) => {
       this.carrouselPartenaires = data;
+      this.groupedPartenaires = this.chunkArray(data, 3);
     });
+  }
+
+  chunkArray(arr: any[], size: number): any[][] {
+    const result = [];
+    for (let i = 0; i < arr.length; i += size) {
+      result.push(arr.slice(i, i + size));
+    }
+    return result;
   }
 }
--- src/app/services/carrousel/carrousel.ts
+++ src/app/services/carrousel/carrousel.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Carrousel {
-  private apiUrl = environment.apiConfig.apiUrl;
-
   constructor(private http: HttpClient) {}
 
-  getPartenaires() {
-    return this.http.get<any[]>(`${this.apiUrl}/carrousel`);
+  getPartenaires(): Observable<any[]> {
+    return this.http.get<any[]>('http://localhost/SAE401/api/carrousel');
   }
 }
